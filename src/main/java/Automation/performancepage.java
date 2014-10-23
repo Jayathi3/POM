8bfe704d549ebbf1f309af5928537410b43621fe
Jayathi3 <jayathipadmaja@gmail.com>
2014-10-23 14:38:37 +0100

    to be reviewed

---
+++ src/main/java/Automation/performancepage.java
@@ -0,0 +1,13 @@
+package Automation;
+
+
+import org.openqa.selenium.By;
+
+public class performancepage extends Baseclass{
+    public void ADDKIP(){
+        driver.findElement(By.id("menu_performance_viewPerformanceModule"));
+
+
+    }
+
+}
